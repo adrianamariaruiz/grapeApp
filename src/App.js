820bef5e622bfb153d9691d3772810d049c8a893
Adriana Ruiz <Adriannaruiz24@gmail.com>
2022-01-25 11:36:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import '../src/index.css';
+import vinedos from './imagenes/vinedos.jpg';
+import uvas from './imagenes/uvas.jpg';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+
+        <div className='header'>nicepage</div>
+
+        <div className='aside-left'>
+          <img src={vinedos} className='vinedos' />
+          <img src={uvas} className='uvas' />
+        </div>
+
+        <div className='aside-right'>
+          <h1 className='titulo'>Growing grapes</h1>
+          <h2>in the home garden</h2>
+          <p>Do you want seedless grapes for fresh eating? Some seedless varieties are being grown in Minnesota now, but, except in far southern Minnesota, all of these varieties will need some winter protection.</p>
+          <br />
+          <button className='boton-learn-more'>
+            LEARN MORE
+          </button>
+        </div>
+
+        <div className='footer'>
+          Template created with Nicepage.
+        </div>
+
+      </div>
     </div>
   );
 }
